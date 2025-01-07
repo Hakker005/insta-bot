9923f48ef9cddd01c0c01346043ddba2b0f830bb
Hakker005 <142396608+Hakker005@users.noreply.github.com>
2025-01-07 22:30:48 +0500

    main.py

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ def download_instagram_video(url: str) -> str:
             if "private" in str(e).lower():
                 raise ValueError("❗️Bu video yopiq akkauntga tegishli bo'lishi mumkin.\n😕Hozirga bu videoni yuklab olish imkoni yo'q.\n👨‍💻Adminlar bu muammo ustida ishlashmoqda!")
             else:
-                raise e
+                raise ValueError("❗️Uzr, yuklab olishda xatolik yuz berdi. Havolani tekshirib qayta urinib ko‘ring.")
 
 # /start komandasi uchun handler
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
@@ -26,30 +26,24 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     user_last_name = update.message.from_user.last_name  # Foydalanuvchining familiyasi (agar bo'lsa)
     
     # Foydalanuvchiga salom yuborish
-    await update.message.reply_text(f"Salom, {user_first_name} {user_last_name if user_last_name else ''}!\n\nBotga xush kelibsiz!")
-
+    await update.message.reply_text(f"Salom, {user_first_name} {user_last_name if user_last_name else ''}!\n\nBotga xush kelibsiz!\n\n🎥 Instagram videolarini yuklab olish uchun video havolasini yuboring.")
 
 # Videoni yuklab olib, foydalanuvchiga jo'natish
 async def download_video(update: Update, context: ContextTypes.DEFAULT_TYPE):
     url = update.message.text
 
     try:
-        # Foydalanuvchi yuborgan havolani o'chirish
-        await update.message.delete()
-
         # Foydalanuvchiga yuklab olish boshlanishini aytish
-        status_message = await update.message.reply_text("⏳Video yuklanmoqda, kuting...")
-
-        # Botning statusini o'zgartirish
-        await update.message.chat.send_action(action="upload_video")
+        status_message = await update.message.reply_text("⏳ Video yuklanmoqda, kuting...")
 
         # Videoni yuklab olish
         video_path = download_instagram_video(url)
 
         # Foydalanuvchiga yuklangan videoni yuborish
-        await update.message.reply_video(
+        await context.bot.send_video(
+            chat_id=update.effective_chat.id,
             video=open(video_path, 'rb'),
-            caption=" 😊Shunchaki foydalaning\n@shoxsan_bot\ndo'stlarga ham ulashing"
+            caption="😊 Videodan rohatlaning!\n\nBot: @shoxsan_bot"
         )
 
         # Yuklangan videoni o'chirish
@@ -59,9 +53,9 @@ async def download_video(update: Update, context: ContextTypes.DEFAULT_TYPE):
         await status_message.delete()
 
     except ValueError as ve:
-        await update.message.reply_text(f"Xatolik: {ve}")
-    except Exception as e:
-        await update.message.reply_text(f"Xatolik yuz berdi: {e}")
+        await update.message.reply_text(str(ve))
+    except Exception:
+        await update.message.reply_text("❗️Uzr, kutilmagan xatolik yuz berdi. Iltimos, qayta urinib ko‘ring.")
 
 # Botni ishga tushirish
 def run_bot():
